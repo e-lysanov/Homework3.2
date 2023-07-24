4d18b35fb09cf9b0d91a0a8c710e08bfe758a228
e-lysanov <e.lysanov@yandex.ru>
2023-07-24 21:46:38 +0300

    15th commit: не работает добавление аватара

--- src/main/java/ru/hogwarts/school/model/Avatar.java
+++ src/main/java/ru/hogwarts/school/model/Avatar.java
@@ -1,9 +1,6 @@
 package ru.hogwarts.school.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.OneToOne;
+import javax.persistence.*;
 
 @Entity
 public class Avatar {
@@ -13,6 +10,7 @@ public class Avatar {
     private String filePath;
     private long fileSize;
     private String mediaType;
+    @Lob
     private byte[] data;
     @OneToOne
     private Student student;
--- src/main/java/ru/hogwarts/school/repository/AvatarRepository.java
+++ src/main/java/ru/hogwarts/school/repository/AvatarRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import ru.hogwarts.school.model.Avatar;
 
 public interface AvatarRepository extends JpaRepository<Avatar, Long> {
-
+    Avatar findAvatarByStudentId(long id);
 }
